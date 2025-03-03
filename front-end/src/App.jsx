95a8fe8c388b9ee0c6334a6a8a2b468505159025
Matthew Lowrey <matthew.s.lowrey@gmail.com>
2025-03-02 18:54:01 -0800

    Navigation + Layout

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,36 +2,43 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from 'react-router-dom'
-
 import './App.css'
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ElixerListPage from './pages/ElixerList';
 import ElixerPage from './pages/ElixerPage';
+import Layout from './Layout';
 
 const routes = [{
   path: '/',
-  element: <HomePage />
-},
-{
-  path: '/about',
-  element: <AboutPage />
-},
-{
-  path: '/elixers',
-  element: <ElixerListPage />
-},
-{
-  path: '/elixers/individual',
-  element: <ElixerPage />
+  element: <Layout />,
+  children: [{
+    path: '/',
+    element: <HomePage />
+  },
+  {
+    path: '/about',
+    element: <AboutPage />
+  },
+  {
+    path: '/elixers',
+    element: <ElixerListPage />
+  },
+  {
+    path: '/elixers/individual',
+    element: <ElixerPage />
+  }]
 }]
 
 const router = createBrowserRouter(routes);
 
 function App() {
   return (
+    <>
     <RouterProvider router={router} />
-  )
+    </>
+
+  );
 }
 
 export default App
