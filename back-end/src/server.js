78bbbc8a2496a7f36c2f023458a2f4d688246df4
Matthew Lowrey <matthew.s.lowrey@gmail.com>
2025-03-11 20:07:04 -0700

    Added MongoDB + Server Connected + Upvote/Comments Linked

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,38 +1,72 @@
 // Start Server Command: npx nodemon src/server.js (script = dev)
 import express from "express";
+import { MongoClient, ReturnDocument, ServerApiVersion } from 'mongodb';
 
-const elixerInfo = [
-    { name: 'elixer-hydration', upvotes: 0, comments: [] },
-    { name: 'elixer-chillout', upvotes: 0, comments: [] },
-    { name: 'elixer-bodyboost', upvotes: 0, comments: [] },
-    { name: 'elixer-relaxation', upvotes: 0, comments: [] },
-    { name: 'elixer-moodboost', upvotes: 0, comments: [] }
-]
+// const elixerInfo = [
+//     { name: 'elixer-hydration', upvotes: 0, comments: [] },
+//     { name: 'elixer-chillout', upvotes: 0, comments: [] },
+//     { name: 'elixer-bodyboost', upvotes: 0, comments: [] },
+//     { name: 'elixer-relaxation', upvotes: 0, comments: [] },
+//     { name: 'elixer-moodboost', upvotes: 0, comments: [] }
+// ]
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/api/elixers/:name/upvote', (req, res) => {
-    const elixer = elixerInfo.find(e => e.name === req.params.name);
-    elixer.upvotes += 1;
+let db;
 
-    //res.send('Success! The elixer ' + req.params.name + ' now has ' + elixer.upvotes + ' upvotes!');
+async function connectToDb() {
+
+    //crows-elixers-connection
+    const uri = 'mongodb://127.0.0.1:27017/';
+
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true
+        }
+    });
+
+    await client.connect();
+
+    db = client.db('CROWSELIXERS');
+
+}
+
+app.get('/api/elixers/:name', async (req, res) => {
+    const { name } = req.params;
+
+    const elixer = await db.collection('ELIXERS').findOne({ name });
+    
     res.json(elixer);
 });
 
-app.post('/api/elixers/:name/comments', (req, res) => {
+app.post('/api/elixers/:name/upvote', async (req, res) => {
     const { name } = req.params;
-    const { postedBy, text } = req.body;
 
-    const elixer = elixerInfo.find(e => e.name === name);
-
-    elixer.comments.push({
-        postedBy,
-        text
+    const updatedElixer = await db.collection('ELIXERS').findOneAndUpdate({ name }, {
+        $inc: { upvotes: 1 }
+    }, {
+        returnDocument: 'after'
     });
 
-    res.json(elixer);
+    res.json(updatedElixer);
+});
+
+app.post('/api/elixers/:name/comments', async (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+    const newComment = { postedBy, text };
+
+    const updatedElixer = await db.collection('ELIXERS').findOneAndUpdate({ name }, {
+        $push: { comments: newComment }
+    }, {
+        returnDocument: 'after'
+    });
+
+    res.json(updatedElixer);
 });
 // app.get('/hello', function(req, res) {
 //     res.send('Hello ' + req.body.name + ' from a GET endpoint!');
@@ -46,6 +80,11 @@ app.post('/api/elixers/:name/comments', (req, res) => {
 //     res.send('Hello ' + req.body.name + ' from a POST endpoint!');
 // });
 
-app.listen(8000, function() {
-    console.log('Server is listening on port 8000');
-});
+async function start() {
+    await connectToDb();
+    app.listen(8000, function() {
+        console.log('Server is listening on port 8000');
+    });
+}
+
+start();
